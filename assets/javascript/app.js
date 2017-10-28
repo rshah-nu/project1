43f21d1b7591e7f796936f5985f80edb86acfef3
rshah-nu <ronakshah2013@u.northwestern.edu>
2017-10-28 02:34:30 -0500

    App mostly functional, numerous bugs and poor code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -44,6 +44,7 @@ $(document).ready(function(){
         divTwo.append(zipLabel);
         $("#searchFields").html(divOne);
         $("#searchFields").append(divTwo);
+        $("#resultsDiv").hide();
     };
     defaultPage();
 
@@ -65,8 +66,46 @@ $(document).ready(function(){
             var baseURL = 'https://data.cityofchicago.org/resource/cwig-ma7x.json?';
             var queryParam = "$where=inspection_date between '2012-01-10T12:00:00' and '2017-01-14T14:00:00' and starts_with(dba_name, upper('" + cuisineInput + "')) and zip='" + zipInput + "'"
             var fullURL = baseURL + queryParam
-            $.getJSON(fullURL, function(result){
-                console.log(result);
+            $.getJSON(fullURL, function(results){
+                console.log(results);
+                console.log(results[0].address);
+                var highRisk = 0;
+                var mediumRisk = 0;
+                var lowRisk = 0;
+                for (var index = 0; index < results.length; index++) {
+                    var riskType = results[index].risk;
+                    switch (riskType) {
+                        case "Risk 1 (High)":
+                            highRisk++
+                            break;
+                        case "Risk 2 (Medium)":
+                            mediumRisk++
+                            break;
+                        case "Risk 3 (Low)":
+                            lowRisk++
+                            break;
+                        default:
+                            break;
+                    };
+                    var tableRow = $("<tr>");
+                    var tableData1 = $("<td>");
+                    var tableData2 = $("<td>");
+                    var tableData3 = $("<td>");
+                    var tableData4 = $('<td>');
+                    tableData1.text(results[index].risk);
+                    tableData2.text(results[index].address);
+                    tableData3.text(results[index].results);
+                    tableData4.text(results[index].violations);
+                    tableRow.append(tableData1);
+                    tableRow.append(tableData2);
+                    tableRow.append(tableData3);
+                    tableRow.append(tableData4);
+                    $("#sampleTable").append(tableRow);
+                };
+                $("#highRisks").text(highRisk);
+                $("#mediumRisks").text(mediumRisk);
+                $("#lowRisks").text(lowRisk);
+                $("#resultsDiv").show();
             });
         };
 
