0972f97ceec67e3467e348531ca81aea265b3e27
Ronak Shah <ronakshah2013@u.northwestern.edu>
2017-10-29 21:35:20 -0500

    Functional City of Chicago API call and frontend print

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
     function defaultPage(){
         $(".cuisine-search-field").hide();
-        $("#resultsDiv").hide();
+        $("#resultsDiv").show();
     }
 
     defaultPage();
@@ -43,7 +43,58 @@ $(document).ready(function(){
         }
         else{
             console.log(zipName);
+            chicagoCall(restaurantName, zipName);
         }
         
     });
+
+    function chicagoCall(restaurantName, zipName){
+        var baseURL = 'https://data.cityofchicago.org/resource/cwig-ma7x.json';
+        var queryURL = "?$where=inspection_date between '2012-01-10T12:00:00' and '2017-01-14T14:00:00' and starts_with(dba_name, upper('" + restaurantName + "')) and zip='" + zipName + "'";
+        var fullURL = baseURL + queryURL;
+        $.getJSON(fullURL, function(r){
+            console.log(r);
+            var pass = 0;
+            var fail = 0;
+            for (var i = 0; i < r.length; i++) {
+                var result = r[i].results;
+                switch (result) {
+                    case "Pass":
+                        pass++
+                        var passTableRow = $("<tr>");
+                        var passTableBody = $("#passTableBody");
+                        var tableData1 = $("<td>");
+                        var tableData2 = $("<td>");
+                        var tableData3 = $("<td>");
+                        var tableData4 = $('<td>');
+                        tableData1.text(r[i].inspection_date);
+                        tableData2.text(r[i].results);
+                        tableData3.text(r[i].inspection_type);
+                        tableData4.text(r[i].violations);
+                        passTableRow.append(tableData1, tableData2, tableData3, tableData4);
+                        passTableBody.append(passTableRow);
+                        break;
+                    case "Fail":
+                        fail++;
+                        var failTableRow = $("<tr>");
+                        var failTableBody = $("#failTableBody");
+                        var tableData1 = $("<td>");
+                        var tableData2 = $("<td>");
+                        var tableData3 = $("<td>");
+                        var tableData4 = $('<td>');
+                        tableData1.text(r[i].inspection_date);
+                        tableData2.text(r[i].results);
+                        tableData3.text(r[i].inspection_type);
+                        tableData4.text(r[i].violations.toLowerCase());
+                        failTableRow.append(tableData1, tableData2, tableData3, tableData4);
+                        failTableBody.append(failTableRow);
+                        break;
+                    default:
+                        console.log("There has been an error with this restaurant");
+                        break;
+                };
+                
+            };
+        });
+    };
 });
