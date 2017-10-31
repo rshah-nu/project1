d0007036bbf593240b34a8a8dd74eddb381bc028
Ronak Shah <ronakshah2013@u.northwestern.edu>
2017-10-31 00:48:33 -0500

    Functional app on new style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,16 +18,61 @@ $(document).ready(function(){
     });
     $("#submitBtn").on("click", function() {
         var restaurantName = $("#rest-search-input").val();
-        console.log(restaurantName);
         var cuisineName = $("#cuisine-search-input").val();
-        console.log(cuisineName);
         var zipName = $("#zip-search-input").val();
         var isValidZip = /(^\d{5}$)/.test(zipName);
-        console.log(zipName);
         if (!isValidZip) {
             Materialize.toast('Please enter a valid five digit zip!', 4000)
         } else {
-            console.log(zipName + " has a valid length");
+            chicagoCall(restaurantName, zipName);
         }
     });
+
+    function chicagoCall(restaurantName, zipName){
+        var baseURL = 'https://data.cityofchicago.org/resource/cwig-ma7x.json';
+        var queryURL = "?$where=inspection_date between '2012-01-10T12:00:00' and '2017-01-14T14:00:00' and starts_with(dba_name, upper('" + restaurantName + "')) and zip='" + zipName + "'";
+        var fullURL = baseURL + queryURL;
+        $.getJSON(fullURL, function(r){
+            var pass = 0;
+            var fail = 0;
+            for (var i = 0; i < r.length; i++) {
+                var result = r[i].results;
+                switch (result) {
+                    case "Pass":
+                        pass++;
+                        var passTableRow = $("<tr>");
+                        var tableData1 = $("<td>");
+                        var tableData2 = $("<td>");
+                        var tableData3 = $("<td>");
+                        var tableData4 = $('<td>');
+                        tableData1.text(r[i].inspection_date);
+                        tableData2.text(r[i].results);
+                        tableData3.text(r[i].inspection_type);
+                        tableData4.text(r[i].violations);
+                        var passTableBody = $("#passTableBody");
+                        passTableRow.append(tableData1, tableData2, tableData3, tableData4);
+                        passTableBody.append(passTableRow);
+                        break;
+                    case "Fail":
+                        fail++;
+                        var failTableRow = $("<tr>");
+                        var failTableBody = $("#failTableBody");
+                        var tableData1 = $("<td>");
+                        var tableData2 = $("<td>");
+                        var tableData3 = $("<td>");
+                        var tableData4 = $('<td>');
+                        tableData1.text(r[i].inspection_date);
+                        tableData2.text(r[i].results);
+                        tableData3.text(r[i].inspection_type);
+                        tableData4.text(r[i].violations);
+                        failTableRow.append(tableData1, tableData2, tableData3, tableData4);
+                        failTableBody.append(failTableRow);
+                        break;
+                    default:
+                        console.log("There has been an error with this restaurant");
+                        break;
+                };
+            };
+        });
+    };
 });
